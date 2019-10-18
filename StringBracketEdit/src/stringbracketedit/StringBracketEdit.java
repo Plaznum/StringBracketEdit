33f2182628796804ddd60961267db9007cd49473
Yelloninja <39409288+Plaznum@users.noreply.github.com>
2019-10-18 10:04:19 -0400

    Add files via upload

---
+++ StringBracketEdit/src/stringbracketedit/StringBracketEdit.java
@@ -0,0 +1,35 @@
+
+package stringbracketedit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yelloninja
+ */
+public class StringBracketEdit {
+
+    
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        
+        String fileName = c.nextLine();
+        String newfileName = "";
+        boolean inBracket = false;
+        for(int i = 0; i<fileName.length(); i++){
+            if(fileName.charAt(i)=='['){
+                inBracket = true;
+            }else if(fileName.charAt(i)== ']' && inBracket == true){
+                inBracket = false;
+                i++;
+            }
+            
+            if(!inBracket)
+                newfileName = newfileName + fileName.charAt(i);
+        }
+        
+        
+        System.out.println("Rename is : " + newfileName);
+    }
+    
+}
